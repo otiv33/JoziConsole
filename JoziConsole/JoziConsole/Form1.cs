d46aab13dc566c929db7bd63276c4465f13708f6
Vito Abeln <vito.abeln@gmail.com>
2020-02-25 11:30:34 +0100

    "Created project"

---
+++ JoziConsole/JoziConsole/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JoziConsole
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileName;
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.ShowDialog();
+            fileName = fd.FileName;
+            Console.Write(fileName);
+        }
+    }
+}
